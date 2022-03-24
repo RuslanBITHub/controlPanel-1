8a0998a5dc1ea51b860dcbe7a1094274a657c1c0
Rodion <50911976+Tequila1Boy@users.noreply.github.com>
2022-03-24 13:57:58 +0300

    Initial commit

---
+++ src/components/component.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="component">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'component',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
